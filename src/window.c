2107ab84c6a3e4593ea18f8186c46c1ba703b8e2
septumfunk <jfleischman6@gmail.com>
2025-11-28 21:08:57 -0800

    Update window.c

--- src/window.c
+++ src/window.c
@@ -61,8 +61,8 @@ sf_window_ex sf_window_new(const sf_str title, const sf_vec2 size, sf_camera *ca
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
     glfwWindowHint(GLFW_RESIZABLE, (hints & SF_WINDOW_RESIZABLE) == SF_WINDOW_RESIZABLE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     if (!((win->handle = glfwCreateWindow((int)size.x, (int)size.y, title.c_str, NULL, NULL))))
         return sf_window_ex_err(SF_GLFW_CREATE_FAILED);
